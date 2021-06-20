0f306b8d20dbcd1df44c08bb275978eda65b7fa5
Julian Dunskus <julian.dunskus@gmail.com>
2021-06-20 02:03:07 +0200

    update to support new valorant-api.com release

--- Shared/Assets/Concrete Assets/PlayerTitleAssets.swift
+++ Shared/Assets/Concrete Assets/PlayerTitleAssets.swift
@@ -14,17 +14,11 @@ private struct PlayerTitleInfoRequest: AssetRequest {
 }
 
 struct PlayerTitleInfo: AssetItem, Codable, Identifiable {
-	var id: PlayerTitle.ID
+	private var uuid: ID
+	var id: PlayerTitle.ID { uuid }
 	/// This is `" "` (a singular space) for the default title.
 	var displayName: String
 	/// This is `nil` for the default title.
 	var titleText: String?
 	var isHiddenIfNotOwned: Bool
-	
-	private enum CodingKeys: String, CodingKey {
-		case id = "uuid"
-		case displayName
-		case titleText
-		case isHiddenIfNotOwned = "isbHiddenIfNotOwned" // lmao
-	}
 }
