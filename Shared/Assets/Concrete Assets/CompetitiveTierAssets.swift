7e5c07c884cff584c8262be9691b6e47367a4853
Julian Dunskus <julian.dunskus@gmail.com>
2021-07-08 02:55:34 +0200

    add competitive tier background color

--- Shared/Assets/Concrete Assets/CompetitiveTierAssets.swift
+++ Shared/Assets/Concrete Assets/CompetitiveTierAssets.swift
@@ -17,18 +17,11 @@ struct CompetitiveTier: AssetItem, Codable {
 	var number: Int
 	var name: String
 	var division: String
-	/// The tier's primary color, as an RGBA hex string. Check out ``color``!
-	var colorString: String
 	
 	/// The tier's primary color.
-	var color: Color? {
-		guard let raw = UInt32(colorString, radix: 16) else { return nil }
-		return Color(
-			red: Double(raw >> 24 & 0xFF) / 255,
-			green: Double(raw >> 16 & 0xFF) / 255,
-			blue: Double(raw >> 8 & 0xFF) / 255
-		).opacity(Double(raw >> 0 & 0xFF) / 255)
-	}
+	@HexEncodedColor var color: Color?
+	/// The tier's background color.
+	@HexEncodedColor var backgroundColor: Color?
 	
 	var icon: AssetImage?
 	var rankTriangleDownwards: AssetImage?
@@ -44,7 +37,8 @@ struct CompetitiveTier: AssetItem, Codable {
 		case number = "tier"
 		case name = "tierName"
 		case division = "divisionName"
-		case colorString = "color"
+		case color
+		case backgroundColor
 		
 		case icon = "largeIcon"
 		case rankTriangleDownwards = "rankTriangleDownIcon"
@@ -65,5 +59,34 @@ struct CompetitiveTier: AssetItem, Codable {
 			case internalName = "assetObjectName"
 			case tiers
 		}
+	}
+}
+
+@propertyWrapper
+struct HexEncodedColor: Codable {
+	let wrappedValue: Color?
+	
+	let hex: String
+	
+	init(from decoder: Decoder) throws {
+		let container = try decoder.singleValueContainer()
+		hex = try container.decode(String.self)
+		wrappedValue = .init(hex: hex)
+	}
+	
+	func encode(to encoder: Encoder) throws {
+		var container = encoder.singleValueContainer()
+		try container.encode(hex)
+	}
+}
+
+private extension Color {
+	init?(hex: String) {
+		guard let raw = UInt32(hex, radix: 16) else { return nil }
+		self = Color(
+			red: Double(raw >> 24 & 0xFF) / 255,
+			green: Double(raw >> 16 & 0xFF) / 255,
+			blue: Double(raw >> 8 & 0xFF) / 255
+		).opacity(Double(raw >> 0 & 0xFF) / 255)
 	}
 }
