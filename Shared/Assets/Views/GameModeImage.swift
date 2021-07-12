978889a5f68bae6e4bafcd78f0552058f4850f9d
Julian Dunskus <julian.dunskus@gmail.com>
2021-07-12 22:40:44 +0200

    add game mode icon view

---
+++ Shared/Assets/Views/GameModeImage.swift
@@ -0,0 +1,35 @@
+import SwiftUI
+import ValorantAPI
+
+struct GameModeImage: View {
+	var id: GameMode.ID
+	
+	@Environment(\.assets) private var assets
+	
+	var body: some View {
+		assets?.gameModes[id]
+			.flatMap(\.displayIcon)?
+			.imageOrPlaceholder(renderingMode: .template)
+	}
+}
+
+#if DEBUG
+struct GameModeImage_Previews: PreviewProvider {
+	static var previews: some View {
+		VStack {
+			ForEach([
+				GameMode.ID.standard, .spikeRush, .deathmatch,
+				.snowballFight, .escalation, .replication,
+				.onboarding, .practice,
+			], id: \.self) { mode in
+				GameModeImage(id: mode)
+				Text(mode.rawValue)
+				Divider()
+			}
+		}
+		.fixedSize()
+		.padding()
+		.previewLayout(.sizeThatFits)
+	}
+}
+#endif
