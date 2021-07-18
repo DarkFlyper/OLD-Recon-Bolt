2132fb5d394a5d6541e8b41a1ba7ab55d965f591
Julian Dunskus <julian.dunskus@gmail.com>
2021-07-19 01:57:40 +0200

    show act rank in summary cell

--- Shared/Views/Match List/CompetitiveSummaryCell.swift
+++ Shared/Views/Match List/CompetitiveSummaryCell.swift
@@ -7,39 +7,88 @@ struct CompetitiveSummaryCell: View {
 	
 	@Environment(\.assets) private var assets
 	
+	private let artworkSize = 96.0
+	private let primaryFont = Font.callout.weight(.semibold)
+	private let secondaryFont = Font.caption
+	
 	var body: some View {
-		content
-			.frame(maxWidth: .infinity)
-			.padding()
+		HStack {
+			currentActInfo
+				.fixedSize() // TODO: remove once text no longer gets cut off without
+			
+			let acts = assets?.seasons.actsInOrder
+			let mostRecentActInfo = acts?
+				.compactMap { act in summary.competitiveInfo?.bySeason?[act.id].map { (act, $0) } }
+				.last { $0.1.winCount > 0 }
+			if let (act, info) = mostRecentActInfo {
+				Spacer()
+				
+				actRankInfo(for: info, in: act)
+			}
+		}
+		.frame(maxWidth: .infinity)
+		.fixedSize(horizontal: false, vertical: true)
+		.padding()
 	}
 	
 	@ViewBuilder
-	private var content: some View {
+	private var currentActInfo: some View {
 		VStack {
 			let act = assets?.seasons.currentAct()
 			let info = act.flatMap { summary.competitiveInfo?.bySeason?[$0.id] }
 			let tierInfo = assets?.seasons.tierInfo(number: info?.competitiveTier ?? 0, in: act)
 			
-				.frame(width: 96, height: 96)
 			RankInfoView(summary: summary, lineWidth: 6, shouldFallBackOnPrevious: false)
+				.frame(height: artworkSize)
 			
 			if let tierInfo = tierInfo {
 				Text(tierInfo.name)
-					.font(.callout.weight(.semibold))
+					.font(primaryFont)
 			}
 			
 			Text("\(info?.rankedRating ?? 0) RR")
-				.font(.caption)
+				.font(secondaryFont)
+		}
+	}
+	
+	@ViewBuilder
+	private func actRankInfo(for info: CompetitiveSummary.SeasonInfo, in act: Act) -> some View {
+		VStack {
+			let tierInfo = assets?.seasons.tierInfo(number: info.competitiveTier, in: act)
+			
+			ActRankView(seasonInfo: info)
+				.frame(height: artworkSize)
+				.overlay(
+					tierInfo?.icon?.imageOrPlaceholder()
+						.frame(width: 32, height: 32)
+						.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
+				)
+			
+			Text(act.name)
+				.font(primaryFont)
+			
+			if let episode = act.episode {
+				Text(episode.name)
+					.font(secondaryFont)
+			}
 		}
 	}
 }
 
 #if DEBUG
 struct CompetitiveSummaryCell_Previews: PreviewProvider {
+	static let assets = AssetManager.forPreviews.assets!
+	static let act = assets.seasons.currentAct()!
+	
 	static var previews: some View {
 		CompetitiveSummaryCell(summary: PreviewData.summary)
-			.padding()
 			.inEachColorScheme()
+			.previewLayout(.sizeThatFits)
+		
+		CompetitiveSummaryCell(summary: PreviewData.summary <- {
+			$0.competitiveInfo!.bySeason = $0.competitiveInfo!.bySeason!
+				.filter { $0.key == act.id }
+		})
 			.previewLayout(.sizeThatFits)
 	}
 }
