324a917dcc6e2780a04878b91cc04604383be0ac
Julian Dunskus <julian.dunskus@gmail.com>
2021-07-24 05:42:36 +0200

    show start time & duration in match details view

--- Shared/Views/Match Details/MatchDetailsView.swift
+++ Shared/Views/Match Details/MatchDetailsView.swift
@@ -1,26 +1,51 @@
 import SwiftUI
 import ValorantAPI
+import HandyOperators
 
 struct MatchDetailsView: View {
 	let data: MatchViewData
 	@State var highlight = PlayerHighlightInfo()
 	
+	private static let startDateFormatter = DateFormatter() <- {
+		$0.dateStyle = .short
+	}
+	private static let startTimeFormatter = DateFormatter() <- {
+		$0.timeStyle = .short
+	}
+	private static let gameLengthFormatter = DateComponentsFormatter() <- {
+		$0.unitsStyle = .abbreviated
+		$0.allowedUnits = [.hour, .minute, .second]
+	}
+	
 	var body: some View {
 		ScrollView {
-			VStack(spacing: 0) {
+			VStack(spacing: 20) {
 				MatchDetailsHero(data: data)
 					.edgesIgnoringSafeArea(.horizontal)
 				
-				Group {
-					ScoreboardView(data: data, highlight: $highlight)
+				HStack {
+					let matchInfo = data.details.matchInfo
 					
-					if KillBreakdownView.canDisplay(for: data) {
-						Divider().padding(.horizontal)
-						
-						KillBreakdownView(data: data, highlight: $highlight)
-					}
+					Text(matchInfo.gameStart, formatter: Self.startDateFormatter)
+					Text(matchInfo.gameStart, formatter: Self.startTimeFormatter)
+						.foregroundColor(.secondary)
+					
+					Spacer()
+					
+					Text(Self.gameLengthFormatter.string(from: matchInfo.gameLength)!)
+					
 				}
-				.padding(.top)
+				.padding(.horizontal)
+				
+				Divider().padding(.horizontal)
+				
+				ScoreboardView(data: data, highlight: $highlight)
+				
+				if KillBreakdownView.canDisplay(for: data) {
+					Divider().padding(.horizontal)
+					
+					KillBreakdownView(data: data, highlight: $highlight)
+				}
 			}
 			.padding(.bottom)
 		}
