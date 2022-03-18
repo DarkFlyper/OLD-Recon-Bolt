1a38de3eff3395cb2b315a651c5cd18066e28567
Julian Dunskus <julian.dunskus@gmail.com>
2022-03-18 22:43:33 +0100

    move some helpers

--- Shared/Helpers/SwiftUI+Helpers.swift
+++ Shared/Helpers/SwiftUI+Helpers.swift
@@ -8,6 +8,29 @@ var isInSwiftUIPreview: Bool {
 let isInSwiftUIPreview = false
 #endif
 
+extension Color {
+	static let separator = Color(.separator)
+}
+
+extension View {
+	@ViewBuilder
+	func withToolbar(allowLargeTitles: Bool = true) -> some View {
+		NavigationView {
+			self.if(!allowLargeTitles) {
+				$0.navigationBarTitleDisplayMode(.inline)
+			}
+		}
+		.navigationViewStyle(.stack)
+	}
+}
+
+extension Image {
+	init?(at url: URL) {
+		guard let image = UIImage(contentsOfFile: url.path) else { return nil }
+		self.init(uiImage: image)
+	}
+}
+
 extension ShapeStyle where Self == Color {
 	// not sure why apple missed this tbh
 	static var accentColor: Self { Color.accentColor }
