48bd95f587adc124dd03e3d874b8e91482c02cec
Julian Dunskus <julian.dunskus@gmail.com>
2022-11-01 18:42:31 +0100

    fix upcoming missions being janky at the beginning of an act

--- Shared/Views/Live/Root/ContractDetailsView.swift
+++ Shared/Views/Live/Root/ContractDetailsView.swift
@@ -138,27 +138,34 @@ struct ContractDetailsView: View {
 	}
 	
 	private func upcomingMissions() -> [MissionInfo]? {
+		// the weekly checkpoint is equal to the activation date of the last completed group of weeklies
+		// so we'll figure that out and find all missions starting after that date
+		
 		guard
 			let assets,
+			// weekly checkpoint may still be in the last act—let's take the later of that and the current act's start
 			let checkpointDate = [
-				// weekly checkpoint may still be in the last act—let's take the later of that and the current act's start
-				details.missionMetadata.weeklyCheckpoint,
-				assets.seasons.currentAct()?.timeSpan.start,
+				details.missionMetadata.weeklyCheckpoint?
+					.adding(days: 7), // add one week to skip the completed group
+				assets.seasons.currentAct()?.timeSpan.start
 			].compacted().max()
 		else { return nil }
 		
-		let checkpoint = Calendar.current.date(
-			byAdding: .day,
-			value: 10, // overshoot by a bit—7 days might cause weird issues with DST and stuff
-			// the weekly checkpoint is one week too early for us—we want the missions after the ones we're currently on
-			to: checkpointDate
-		)!
+		// shift forward by 3 days to skip the currently-active group and into the middle of the week
+		// this also avoids issues with acts launching at different times in different regions, as well as DST
+		let checkpoint = checkpointDate.adding(days: 3)
 		
 		return assets.missions.values
 			.filter { $0.type == .weekly }
 			// all weeklies have activation dates
-			.filter { $0.activationDate! >= checkpoint }
+			.filter { $0.activationDate! > checkpoint }
 			.sorted(on: \.activationDate!)
+	}
+}
+
+private extension Date {
+	func adding(days: Double) -> Self {
+		addingTimeInterval(days * 24 * 3600) // don't need to consider leap seconds or DST for this because it's pretty rough anyway
 	}
 }
 
