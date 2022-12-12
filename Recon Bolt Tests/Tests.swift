5d76c8e5f0f45dd47677ff609df87a9eb81efdbb
Julian Dunskus <julian.dunskus@gmail.com>
2022-12-12 01:52:07 +0100

    update asset test
    man the protoquest rework really paid off here

--- Recon Bolt Tests/Tests.swift
+++ Recon Bolt Tests/Tests.swift
@@ -1,14 +1,17 @@
 import XCTest
 import HandyOperators
+import Protoquest
 @testable import Recon_Bolt
 
 final class Tests: XCTestCase {
-	private static let session = URLSession(configuration: URLSessionConfiguration.default <- {
-		$0.protocolClasses!.insert(ImageIgnoringProtocol.self, at: 0)
-	})
+	private static let layer = Protolayer.urlSession().readRequest { request in
+		if request.url?.pathExtension == "png" {
+			throw IgnoredRequestError.image
+		}
+	}
 	
 	func testDownloadingAssets() async throws {
-		let client = AssetClient(session: Self.session)
+		let client = AssetClient(networkLayer: Self.layer)
 		let version = try await client.getCurrentVersion()
 		let assetCollection = try await client.collectAssets(for: version)
 		
@@ -23,6 +26,10 @@ final class Tests: XCTestCase {
 		//print(String(bytes: encoded, encoding: .utf8)!)
 		XCTAssertNoThrow(try JSONDecoder().decode(AssetCollection.self, from: encoded))
 	}
+	
+	enum IgnoredRequestError: Error {
+		case image
+	}
 }
 
 extension XCTestCase {
@@ -31,26 +38,4 @@ extension XCTestCase {
 		measure(options: options) { result = .init(catching: block) }
 		return try result!.get()
 	}
-}
-
-final class ImageIgnoringProtocol: URLProtocol {
-	override class func canInit(with task: URLSessionTask) -> Bool {
-		task.currentRequest.map(canInit(with:)) ?? false
-	}
-	
-	override class func canInit(with request: URLRequest) -> Bool {
-		request.url?.pathExtension == "png"
-	}
-	
-	override class func canonicalRequest(for request: URLRequest) -> URLRequest {
-		request
-	}
-	
-	override func startLoading() {
-		client?.urlProtocol(self, didReceive: HTTPURLResponse(), cacheStoragePolicy: .notAllowed)
-		client?.urlProtocol(self, didLoad: Data())
-		client?.urlProtocolDidFinishLoading(self)
-	}
-	
-	override func stopLoading() {}
 }
