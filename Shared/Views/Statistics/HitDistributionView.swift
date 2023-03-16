1335f13ecb8424c4a4464efea91b568a598315f4
Julian Dunskus <julian.dunskus@gmail.com>
2023-03-17 00:15:17 +0100

    visual tweaks

--- Shared/Views/Statistics/HitDistributionView.swift
+++ Shared/Views/Statistics/HitDistributionView.swift
@@ -25,7 +25,6 @@ struct HitDistributionView: View {
 	func distributionGrid(for tally: Tally) -> some View {
 		Grid(horizontalSpacing: 20, verticalSpacing: 4) {
 			distributionRow("Head", count: tally.headshots, from: tally)
-				.foregroundColor(.valorantRed)
 			distributionRow("Body", count: tally.bodyshots, from: tally)
 			distributionRow("Legs", count: tally.legshots, from: tally)
 			distributionRow("Total", count: tally.total, from: tally)
@@ -57,7 +56,8 @@ struct HitDistributionView: View {
 		} header: {
 			Text("By Weapon")
 		} footer: {
-			// TODO: disclaimer about insufficient data
+			Text("Unfortunately, Valorant doesn't store information about which weapon was used for each hit. All we get is the weapon you started the round with and what weapon you used for each kill. Recon Bolt guesses the weapon involved in each damage based on this information, but it cannot be perfect (and neither can any other tracker).")
+				.frame(maxWidth: .infinity, alignment: .leading)
 		}
 	}
 	
@@ -75,33 +75,10 @@ struct HitDistributionView: View {
 				WeaponImage.killStreamIcon(weapon)
 					.frame(maxHeight: weaponIconHeight)
 			}
-			.foregroundStyle(.secondary)
+			//.foregroundStyle(.secondary)
+			.foregroundColor(.valorantRed)
 			
 			distributionGrid(for: tally)
-			/*
-			EqualHStack(columnAlignment: .trailing, spacing: 8) {
-				VStack {
-					Text("\(tally.legshots) hits")
-						.foregroundStyle(.secondary)
-					percentageLabel(count: tally.legshots, total: tally.total)
-				}
-				
-				VStack {
-					Text("\(tally.bodyshots) hits")
-						.foregroundStyle(.secondary)
-					percentageLabel(count: tally.bodyshots, total: tally.total)
-				}
-				
-				VStack {
-					Text("\(tally.headshots) hits")
-						.foregroundStyle(.secondary)
-					percentageLabel(count: tally.headshots, total: tally.total)
-				}
-				
-				Text("\(tally.total) total")
-					.foregroundStyle(.secondary)
-			}
-			 */
 		}
 	}
 	
@@ -110,64 +87,12 @@ struct HitDistributionView: View {
 	}
 }
 
-import CGeometry
-
-@available(iOS 16.0, *)
-struct EqualHStack: Layout {
-	var columnAlignment: HorizontalAlignment = .center
-	var spacing: CGFloat = 8
-	
-	var anchor: UnitPoint {
-		switch columnAlignment {
-		case .leading:
-			return .leading
-		case .center:
-			return .center
-		case .trailing:
-			return .trailing
-		default:
-			return .center
-		}
-	}
-	
-	func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) -> CGSize {
-		let subproposal = ProposedViewSize(
-			width: proposal.width.map { $0 / .init(subviews.count) },
-			height: proposal.height
-		)
-		let sizes = subviews.map { $0.sizeThatFits(subproposal) }
-		return CGSize(
-			width: proposal.width ?? (sizes.map(\.width).reduce(0, +) + spacing * .init(subviews.count - 1)),
-			height: sizes.map(\.height).max() ?? 0
-		)
-	}
-	
-	func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) {
-		let subwidth = (bounds.width - spacing * .init(subviews.count - 1)) / .init(subviews.count)
-		let subproposal = ProposedViewSize(
-			width: subwidth,
-			height: proposal.height
-		)
-		let anchor = self.anchor
-		let stride = subwidth + spacing
-		for (i, view) in subviews.enumerated() {
-			view.place(
-				at: bounds.origin + CGVector(
-					dx: subwidth * anchor.x + stride * .init(i),
-					dy: bounds.height * anchor.y
-				),
-				anchor: anchor,
-				proposal: subproposal
-			)
-		}
-	}
-}
-
 #if DEBUG
 @available(iOS 16.0, *)
 struct HitDistributionView_Previews: PreviewProvider {
 	static var previews: some View {
 		HitDistributionView(statistics: PreviewData.statistics)
+			.withToolbar()
 	}
 }
 #endif
